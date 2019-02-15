07608af5c8f5cb2cc3b6f7ea42b13d136f52563c
Max <max.dez@live.co.za>
2019-02-15 19:34:08 +0200

    Add files

---
+++ main.js
@@ -0,0 +1,9 @@
+import Discord from 'discord.js'
+import secrets from './secrets'
+
+const client = new Discord.Client()
+
+client.once('ready', () => console.log(`Logged in as ${client.user.tag}.`))
+client.on('message', msg => msg.content.toLowerCase() === '!ping' && msg.reply('Pong!'))
+
+client.login(secrets.token)
