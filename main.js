0662f63bc588e218544a68ab13c5313dc9fffbf7
Max <max.dez@live.co.za>
2019-02-15 22:32:21 +0200

    Store Discord API token correctly

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 import Discord from 'discord.js'
-import secrets from './secrets'
 
 const client = new Discord.Client()
 
 client.once('ready', () => console.log(`Logged in as ${client.user.tag}.`))
 client.on('message', msg => msg.content.toLowerCase() === '!ping' && msg.reply('Pong!'))
 
-client.login(secrets.token || process.env.token)
+// You must have added your Discord application client token as an environment
+// variable called 'TOKEN'. You should do this by adding this token as a Now secret
+// with `$ now secrets add discord-token "mysecretdiscordtoken"` and making sure your now.json
+// exposes this secret as an environment variable called `TOKEN`. Example now.json snippet:
+// "env": {
+//     "TOKEN": "@discord-token"
+// }
+client.login(process.env.TOKEN)
+
+// This lets Now know that we want this program to continue running and not
+// just execute once then terminate.
+require('http').createServer().listen(3000)
+
