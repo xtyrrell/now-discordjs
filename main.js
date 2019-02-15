7c01ad3b3c14acc30cb6338445b85cb1d1e4576e
Max <max.dez@live.co.za>
2019-02-15 19:44:00 +0200

    Read secret token as an environment variable

--- main.js
+++ main.js
@@ -6,4 +6,4 @@ const client = new Discord.Client()
 client.once('ready', () => console.log(`Logged in as ${client.user.tag}.`))
 client.on('message', msg => msg.content.toLowerCase() === '!ping' && msg.reply('Pong!'))
 
-client.login(secrets.token)
+client.login(secrets.token || process.env.token)
